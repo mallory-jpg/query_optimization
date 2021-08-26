d13d6fe2b4d3cde1b209487a37947bf0ec1db143
Mallory <65197541+mallory-jpg@users.noreply.github.com>
2021-08-25 22:37:13 -0500

    Create assignment.sql

---
+++ source/assignment.sql
@@ -0,0 +1,36 @@
+
+-- 1. List the name of the student with id equal to v1 (id).
+SELECT name FROM Student WHERE id = @v1;
+
+-- 2. List the names of students with id in the range of v2 (id) to v3 (inclusive).
+SELECT name FROM Student WHERE id BETWEEN @v2 AND @v3;
+
+-- 3. List the names of students who have taken course v4 (crsCode).
+SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);
+
+-- 4. List the names of students who have taken a course taught by professor v5 (name).
+SELECT name FROM Student,
+	(SELECT studId FROM Transcript,
+		(SELECT crsCode, semester FROM Professor
+			JOIN Teaching
+			WHERE Professor.name = @v5 AND Professor.id = Teaching.profId) as alias1
+	WHERE Transcript.crsCode = alias1.crsCode AND Transcript.semester = alias1.semester) as alias2
+WHERE Student.id = alias2.studId;
+
+-- 5. List the names of students who have taken a course from department v6 (deptId), but not v7.
+SELECT * FROM Student, 
+	(SELECT studId FROM Transcript, Course WHERE deptId = @v6 AND Course.crsCode = Transcript.crsCode
+	AND studId NOT IN
+	(SELECT studId FROM Transcript, Course WHERE deptId = @v7 AND Course.crsCode = Transcript.crsCode)) as alias
+WHERE Student.id = alias.studId;
+
+-- 6. List the names of students who have taken all courses offered by department v8 (deptId).
+SELECT name FROM Student,
+	(SELECT studId
+	FROM Transcript
+		WHERE crsCode IN
+		(SELECT crsCode FROM Course WHERE deptId = @v8 AND crsCode IN (SELECT crsCode FROM Teaching))
+		GROUP BY studId
+		HAVING COUNT(*) = 
+			(SELECT COUNT(*) FROM Course WHERE deptId = @v8 AND crsCode IN (SELECT crsCode FROM Teaching))) as alias
+WHERE id = alias.studId;
